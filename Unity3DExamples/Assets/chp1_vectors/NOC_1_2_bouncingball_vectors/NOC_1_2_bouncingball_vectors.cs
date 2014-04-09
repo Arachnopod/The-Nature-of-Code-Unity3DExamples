3454d1bc546269217f9a218a26b8fba9c7c1a9b6
Joseph Fiola <zef@joefiola.com>
2014-04-09 18:01:24 -0400

    Added Example 1-2: Bouncing Ball, with Vector
    Example 1-2: Bouncing Ball, with Vector converted to C# for Unity3D

---
+++ Unity3DExamples/Assets/chp1_vectors/NOC_1_2_bouncingball_vectors/NOC_1_2_bouncingball_vectors.cs
@@ -0,0 +1,45 @@
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+// Converted to C# for Unity 3D 3.5.7
+// Joseph Fiola
+// http://www.joefiola.com
+
+// Example 1-2: Bouncing Ball, with Vector
+
+using UnityEngine;
+using System.Collections;
+
+public class NOC_1_2_bouncingball_vectors : MonoBehaviour {
+	
+	
+	public Transform target;		//GameObject to bounce around
+	private Vector3 location = new Vector3(0.5f,0.5f,20.0f);	// position of GameObject
+	private Vector3 velocity = new Vector3(0.01f,0.01f,0.0f);		//speed of GameObject
+		public float xspeed = 0.0f;		//adjust x speed in editor inspector window 
+	public float yspeed = 0.0f;		//adjust y speed in editor inspector window 
+	
+	void setup()
+	{
+	}
+
+	
+	void Update () {
+	
+		// Add the current speed to the location.
+		location = location + velocity;
+		
+
+		if (location.x > 1 || location.x < 0){
+			velocity.x = velocity.x * -1.0f;
+		}
+		if (location.y > 1 || location.y < 0){
+			velocity.y = velocity.y * -1.0f;
+		}
+		
+		// Display GameObject at x location
+		target.position = camera.ViewportToWorldPoint(new Vector3(location.x,location.y,location.z));
+	}
+}
+
